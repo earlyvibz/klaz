9e2e1dfe2326ea77cac0c761bd6b9ba685dc38b0
CostierLucas <lucas.costier@outlook.fr>
2025-09-17 16:09:32 +0200

    fix lint

--- apps/front/src/routes/_dashboard.admin.settings.tsx
+++ apps/front/src/routes/_dashboard.admin.settings.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_dashboard/admin/settings')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/_dashboard/admin/settings")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/_dashboard/admin/settings"!</div>
+	return <div>Hello "/_dashboard/admin/settings"!</div>;
 }
